59f5b9c9ac1db3fc337c3eee2397ffbb394db223
Tuan Pham <tuanliz0403@gmail.com>
2024-12-24 17:23:54 +0700

    remove-absolute-path

--- main.ps1
+++ main.ps1
@@ -6,12 +6,12 @@
 # It's your preference.
 # ******************************************************************************
 
-$INIT = "C:\Users\tuanl\OneDrive\Documents\PowerShell\MyModules\INIT.psm1"
-$NAV = "C:\Users\tuanl\OneDrive\Documents\PowerShell\MyModules\NAV.psm1"
-$FILE_QUERRY = "C:\Users\tuanl\OneDrive\Documents\PowerShell\MyModules\FILE_QUERRY.psm1"
-$FILE_MANAGER = "C:\Users\tuanl\OneDrive\Documents\PowerShell\MyModules\FILE_MANAGER.psm1"
-$FUNCTIONS = "C:\Users\tuanl\OneDrive\Documents\PowerShell\MyModules\FUNCTIONS.psm1"
-$C_CPP_COMPILER = "C:\Users\tuanl\OneDrive\Documents\PowerShell\MyModules\C_CPP.psm1"
+$INIT = "~\OneDrive\Documents\PowerShell\MyModules\INIT.psm1"
+$NAV = "~\OneDrive\Documents\PowerShell\MyModules\NAV.psm1"
+$FILE_QUERRY = "~\OneDrive\Documents\PowerShell\MyModules\FILE_QUERRY.psm1"
+$FILE_MANAGER = "~\OneDrive\Documents\PowerShell\MyModules\FILE_MANAGER.psm1"
+$FUNCTIONS = "~\OneDrive\Documents\PowerShell\MyModules\FUNCTIONS.psm1"
+$C_CPP_COMPILER = "~\OneDrive\Documents\PowerShell\MyModules\C_CPP.psm1"
 
 Import-Module $INIT
 Import-Module $FILE_QUERRY
