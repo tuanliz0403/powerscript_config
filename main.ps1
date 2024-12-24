3636fd718a88850ea7a18930a213d9144b11053e
Tuan Pham <tuanliz0403@gmail.com>
2024-12-24 17:44:52 +0700

    Remove comment

--- main.ps1
+++ main.ps1
@@ -17,13 +17,6 @@ $FILE_MANAGER = Join-Path $myModulePath "FILE_MANAGER.ps1"
 $FUNCTIONS = Join-Path $myModulePath "FUNCTIONS.ps1"
 $C_CPP_COMPILER = Join-Path $myModulePath "C_CPP.ps1"
 
-# $INIT = "~\OneDrive\Documents\PowerShell\MyModules\INIT.ps1" 
-# $FILE_QUERRY = "~\OneDrive\Documents\PowerShell\MyModules\FILE_QUERRY.ps1" 
-# $NAV = "~\OneDrive\Documents\PowerShell\MyModules\NAV.ps1" 
-# $FILE_MANAGER = "~\OneDrive\Documents\PowerShell\MyModules\FILE_MANAGER.ps1" 
-# $FUNCTIONS = "~\OneDrive\Documents\PowerShell\MyModules\FUNCTIONS.ps1"
-# $C_CPP_COMPILER = "~\OneDrive\Documents\PowerShell\MyModules\C_CPP.ps1"
-
 . $INIT
 . $FILE_QUERRY
 . $FILE_MANAGER
