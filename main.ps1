969ac11f6c2ecba7cfbb00e97300b6e7e7e99839
Tuan Pham <tuanliz0403@gmail.com>
2024-12-24 17:44:16 +0700

    change psm to ps

--- main.ps1
+++ main.ps1
@@ -6,16 +6,28 @@
 # It's your preference.
 # ******************************************************************************
 
-$INIT = "~\OneDrive\Documents\PowerShell\MyModules\INIT.psm1"
-$NAV = "~\OneDrive\Documents\PowerShell\MyModules\NAV.psm1"
-$FILE_QUERRY = "~\OneDrive\Documents\PowerShell\MyModules\FILE_QUERRY.psm1"
-$FILE_MANAGER = "~\OneDrive\Documents\PowerShell\MyModules\FILE_MANAGER.psm1"
-$FUNCTIONS = "~\OneDrive\Documents\PowerShell\MyModules\FUNCTIONS.psm1"
-$C_CPP_COMPILER = "~\OneDrive\Documents\PowerShell\MyModules\C_CPP.psm1"
+Get the config folder of different devices
+$configPath = Split-Path -Parent $PROFILE
+$myModulePath = Join-Path $configPath "MyModules"
 
-Import-Module $INIT
-Import-Module $FILE_QUERRY
-Import-Module $FILE_MANAGER
-Import-Module $NAV
-Import-Module $FUNCTIONS
-Import-Module $C_CPP_COMPILER
+$INIT = Join-Path $myModulePath "INIT.ps1"
+$NAV = Join-Path $myModulePath "NAV.ps1"
+$FILE_QUERRY = Join-Path $myModulePath "FILE_QUERRY.ps1"
+$FILE_MANAGER = Join-Path $myModulePath "FILE_MANAGER.ps1"
+$FUNCTIONS = Join-Path $myModulePath "FUNCTIONS.ps1"
+$C_CPP_COMPILER = Join-Path $myModulePath "C_CPP.ps1"
+
+# $INIT = "~\OneDrive\Documents\PowerShell\MyModules\INIT.ps1" 
+# $FILE_QUERRY = "~\OneDrive\Documents\PowerShell\MyModules\FILE_QUERRY.ps1" 
+# $NAV = "~\OneDrive\Documents\PowerShell\MyModules\NAV.ps1" 
+# $FILE_MANAGER = "~\OneDrive\Documents\PowerShell\MyModules\FILE_MANAGER.ps1" 
+# $FUNCTIONS = "~\OneDrive\Documents\PowerShell\MyModules\FUNCTIONS.ps1"
+# $C_CPP_COMPILER = "~\OneDrive\Documents\PowerShell\MyModules\C_CPP.ps1"
+
+. $INIT
+. $FILE_QUERRY
+. $FILE_MANAGER
+. $NAV
+. $FUNCTIONS
+. $C_CPP_COMPILER
+
